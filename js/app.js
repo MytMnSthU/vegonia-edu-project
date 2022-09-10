3f6e45140b6079271a8b95d90ab00c227e3cd473
MytMnSthU <myatminsithu0072@gmail.com>
2022-09-10 14:38:42 -0700

    Done Features, Faqs and Rating

--- js/app.js
+++ js/app.js
@@ -7,15 +7,13 @@ $(function () {
 // End jQuery Area
 
 
+// Start Counter
 const countNums = document.querySelectorAll(".vgn-header__counter-number");
-
 
 countNums.forEach((countNum, idx) => {  
     let final = countNum.dataset.count;
     counter(countNum, final);
 });
-
-
 
 function counter(start,end) {
     let intervalId;
@@ -33,3 +31,32 @@ function counter(start,end) {
         }
      },30);
 }
+// End Counter
+
+
+// Start Chart
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ["Task", "Hours per Day"],
+        ["Myanmar", 8],
+        ["Singapore", 6],
+        ["Thailand", 4],
+        ["Indonesia", 4],
+        ["Malaysia", 4],
+    ]);
+
+    var options = {
+        title: "International Students",
+    };
+
+    var chart = new google.visualization.PieChart(
+        document.getElementById("chart")
+    );
+
+    chart.draw(data, options);
+}
+
+// End Chart
